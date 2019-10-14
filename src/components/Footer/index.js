607c99eb44c43a7e4bea46e22f349eb6ccc753b6
Axel <axosar@gmail.com>
2019-10-14 13:19:31 -0500

    Header y footer layout

---
+++ src/components/Footer/index.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import logoblanco from '../../logo-blanco.svg';
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div className="footer-top-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              <div className="footer-rows-container">
+                <div className="footer-rows-left">
+                  <div className="footer-row">
+                    <div className="footer-row-inner footer-logo">
+                      <img src={logoblanco} alt="imcyc" title="imcyc" style={{width: '150px'}} />
+                    </div>
+                  </div>
+                </div>
+                <div className="footer-rows-right">
+                  <div className="footer-row">
+                    <div className="footer-row-inner">
+                      <ul className="footer-social-links">
+                        <li>
+                          <Link to="/" title="Facebook" data-tippy-placement="bottom" data-tippy-theme="light">
+                            <i class="icon-brand-facebook-f"></i>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" title="Twitter" data-tippy-placement="bottom" data-tippy-theme="light">
+                            <i class="icon-brand-twitter"></i>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" title="Google Plus" data-tippy-placement="bottom" data-tippy-theme="light">
+                            <i class="icon-brand-google-plus-g"></i>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link to="/" title="LinkedIn" data-tippy-placement="bottom" data-tippy-theme="light">
+                            <i class="icon-brand-linkedin-in"></i>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="footer-row">
+                    <div className="footer-row-inner">
+                      <select className="selectpicker language-switcher" data-selected-text-format="count" data-size="5">
+                        <option selected>Español</option>
+                        <option>Français</option>
+                        <option>English</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-12">
+              © 2019 <strong>Instituto Mexicano del Cemento y del Concreto A.C.</strong>. Todo Los Derechos Reservados.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import logo from '../../logo.svg';
+
+const Header = () => {
+  return (
+    <header id="header-container" class="fullwidth">
+    <div id="header">
+      <div className="container">
+
+        <div className="left-side">
+          <div id="logo">
+            <Link to="/"><img src={logo} alt=""/></Link>
+          </div>
+          <nav id="navigation">
+					  <ul id="responsive">
+              <li>
+                <Link to="#" className="current">Home</Link>
+              </li>
+              <li>
+                <Link to="#">Empleos</Link>
+              </li>
+              <li>
+                <Link to="#">Candidatos</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="clearfix"></div>
+        </div>
+      
+
+        <div className="right-side">
+          <div className="header-widget hide-on-mobile">
+            <div className="header-notifications">
+              <div className="header-notifications-trigger">
+                <Link to="#"><i class="icon-feather-bell"></i><span>4</span></Link>
+              </div>
+              <div className="header-notifications-dropdown">
+                <div className="header-notifications-headline">
+                  <h4>Notificaciones</h4>
+                  <button className="mark-as-read ripple-effect-dark" title="Mark all as read" data-tippy-placement="left">
+                    <i className="icon-feather-check-square"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="header-notifications">
+              <div className="header-notifications-trigger">
+                <Link to="#"><i class="icon-feather-mail"></i><span>3</span></Link>
+              </div>
+            </div>
+          </div>
+
+          <div className="header-widget">
+            <div className="header-notifications user-menu">
+              <div className="header-notifications-trigger">
+                <Link to="#">
+                  <div className="user-avatar status-online">
+                    <img src="images/user-avatar-small-01.jpg" alt="" />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    </header>
+  );
+}
+ 
+export default Header;
